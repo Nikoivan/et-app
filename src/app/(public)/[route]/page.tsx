2904186691c963bfdf5dcd59368f6156c7a44271
Niko Ivan <nixonivan@rambler.ru>
2025-10-06 21:55:55 +0300

    fix

--- src/app/(public)/[route]/page.tsx
+++ src/app/(public)/[route]/page.tsx
@@ -1,10 +1,17 @@
-'use server';
-
+import { postServices } from '@/features/post/services/post-services';
 import { FC } from 'react';
 import { PostView } from '@/views/post/server';
 
+export const dynamic = 'force-static'; // жёстко SSG
+export const revalidate = false;
+
 const Page: FC<{
   params: Promise<{ route: string }>;
-}> = async props => <PostView {...props} />;
+}> = async ({ params }) => {
+  const { route } = await params;
+  const either = await postServices.getPostByRoute(route);
+
+  return <PostView either={either} />;
+};
 
 export default Page;
--- src/views/post/containers/post-view.tsx
+++ src/views/post/containers/post-view.tsx
@@ -1,29 +1,23 @@
 'use server';
 
 import { FC } from 'react';
-
-import { postServices } from '@/features/post/services/post-services';
 import { PostMain } from '@/views/post/ui/post-main';
 
+import { Either } from '@/shared/lib/either';
+import { PostDomain } from '@/entities/post/server';
+
 type Props = {
-  params: Promise<{
-    route: string;
-  }>;
+  either: Either<string, PostDomain.PostEntity>;
 };
 
-export const PostView: FC<Props> = async ({ params }) => {
-  const { route } = await params;
-  const either = await postServices.getPostByRoute(route);
-
-  return (
-    <>
-      {either.type === 'right' ? (
-        <PostMain {...either.value} />
-      ) : (
-        <div className='flex justify-center items-center'>
-          Ошибка при загрузке страницы... попробуйте вновь
-        </div>
-      )}
-    </>
-  );
-};
+export const PostView: FC<Props> = async ({ either }) => (
+  <>
+    {either.type === 'right' ? (
+      <PostMain {...either.value} />
+    ) : (
+      <div className='flex justify-center items-center'>
+        Ошибка при загрузке страницы... попробуйте вновь
+      </div>
+    )}
+  </>
+);
